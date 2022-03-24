b10ce8a2bb413c2655f51b4b33ffcc9b2396c0da
Shaymaa Ashqar <60795255+Shaymaaashqar@users.noreply.github.com>
2022-03-24 13:01:52 +0200

    files of task

---
+++ array.js
@@ -0,0 +1,6 @@
+
+var array1 = [1,2,3,[1,3]];
+function arrayTesting(){
+      return array1;
+   }
+module.exports=arrayTesting;
---
+++ array.test.js
@@ -0,0 +1,29 @@
+const arrayTesting=require('./array');
+test(" test case",()=>{
+
+    expect(Array.isArray(arrayTesting())).toBe(true);
+  
+});
+
+test(" test case",()=>{
+   
+    expect(arrayTesting()).toContain(1);
+
+})
+
+test(" test case",()=>{
+     expect(arrayTesting().at(2)).toBe(3);
+    
+})
+
+
+test(" test case",()=>{
+    expect(arrayTesting().length).toBe(4);
+   
+})
+
+test(" test case",()=>{
+    expect(arrayTesting().indexOf(1)).toBe(0);
+   
+})
+
